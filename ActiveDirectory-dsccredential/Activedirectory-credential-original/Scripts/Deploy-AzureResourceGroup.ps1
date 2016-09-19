cfd1cbb68c6b134844772361a59f139118de6681
bytezn <lawrancer@rewardsco.co.za>
2016-09-19 17:36:18 +0200

    1

--- ActiveDirectory-dsccredential/Activedirectory-credential-original/Scripts/Deploy-AzureResourceGroup.ps1
+++ ActiveDirectory-dsccredential/Activedirectory-credential-original/Scripts/Deploy-AzureResourceGroup.ps1
@@ -60,7 +60,7 @@ if ($UploadArtifacts) {
     }
 
     $StorageAcc = Get-AzureRmStorageAccountKey -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccountName
-	$StorageAccountKey = "ThXw9KEx7e1y4ui8arZL+EYSiz0CPuLy3+aGYf5vA6X/YVY5SeQsaiTCZET4J5i9b/aNTKRDgEdINVf3DiAm5w=="
+	$StorageAccountKey = "ZHVXoIWBJbGDdM8FvKDWnG0ewSX7VKCEkiffM5Ia1/LkPHH5n5lAv7ER3fhxCAw0dCSkeovmSRjF7Yn/pb/GcA=="
     $StorageAccountContext = (Get-AzureRmStorageAccount -ResourceGroupName $StorageAccountResourceGroupName -Name $StorageAccountName).Context
 
     # Copy Configuration Data files into staging directory
