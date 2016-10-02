6f59cd34928575c923643bae766572bc46edc82d
bytezn <lawrancer@rewardsco.co.za>
2016-10-02 18:24:39 +0200

    1

--- ActiveDirectory-dsccredential/Activedirectory-credential-original/DSC/dscDCConfiguration.ps1
+++ ActiveDirectory-dsccredential/Activedirectory-credential-original/DSC/dscDCConfiguration.ps1
@@ -1,3 +1,150 @@
+Configuration fileserver2
+{
+ 
+[CmdletBinding()]
+ 
+Param (
+    [string] $NodeName,
+    [string] $domainName,
+    [System.Management.Automation.PSCredential]$domainAdminCredentials
+)
+ 
+Import-DscResource -ModuleName PSDesiredStateConfiguration, XComputerManagement, xSmbShare, xdfs
+ 
+Node localhost
+    {
+        LocalConfigurationManager
+        {
+            ConfigurationMode = 'ApplyAndAutoCorrect'
+            RebootNodeIfNeeded = $true
+            ActionAfterReboot = 'ContinueConfiguration'
+            AllowModuleOverwrite = $true
+        }
+ 
+         WindowsFeature fileservice
+        {
+            Name = "fileandstorage-services"
+            Ensure = "Present"
+        } 
+         
+      	File filename
+      	{
+      		DestinationPath           = "c:\1\2.txt"
+      		Contents                  = "myfile"
+      		DependsOn                 = "[WindowsFeature]fileservice"
+      		Ensure                    = "Present"
+      		Force                     = $true
+      		PsDscRunAsCredential      = $domainAdminCredentials
+      		Type                      = "File"
+      	}
+                    
+        xSmbShare myshare 
+        	{
+        		Name                      = "myshare"
+        		Path                      = "c:\1"
+        		DependsOn                 = "[File]filename"
+        		Ensure                    = "Present"
+        		FolderEnumerationMode     = "Unrestricted"
+        		FullAccess                = "everyone"
+        		PsDscRunAsCredential      = $domainAdminCredentials
+        	}
+     		        
+		 WindowsFeature DFS
+        {
+            Name = 'FS-DFS-Namespace'
+            Ensure = 'Present'
+			DependsOn  = "[xSmbShare]myshare"
+        }
+
+       
+     }
+
+}
+
+Configuration fileserver3
+{
+ 
+[CmdletBinding()]
+ 
+Param (
+    [string] $NodeName,
+    [string] $domainName,
+    [System.Management.Automation.PSCredential]$domainAdminCredentials
+)
+ 
+Import-DscResource -ModuleName PSDesiredStateConfiguration, XComputerManagement, xSmbShare, xdfs
+ 
+Node localhost
+    {
+        LocalConfigurationManager
+        {
+            ConfigurationMode = 'ApplyAndAutoCorrect'
+            RebootNodeIfNeeded = $true
+            ActionAfterReboot = 'ContinueConfiguration'
+            AllowModuleOverwrite = $true
+        }
+ 
+         WindowsFeature fileservice
+        {
+            Name = "fileandstorage-services"
+            Ensure = "Present"
+        } 
+         
+      	File filename
+      	{
+      		DestinationPath           = "c:\1\2.txt"
+      		Contents                  = "myfile"
+      		DependsOn                 = "[WindowsFeature]fileservice"
+      		Ensure                    = "Present"
+      		Force                     = $true
+      		PsDscRunAsCredential      = $domainAdminCredentials
+      		Type                      = "File"
+      	}
+                    
+        xSmbShare myshare 
+        	{
+        		Name                      = "myshare"
+        		Path                      = "c:\1"
+        		DependsOn                 = "[File]filename"
+        		Ensure                    = "Present"
+        		FolderEnumerationMode     = "Unrestricted"
+        		FullAccess                = "everyone"
+        		PsDscRunAsCredential      = $domainAdminCredentials
+        	}
+     		        
+		 WindowsFeature DFS
+        {
+            Name = 'FS-DFS-Namespace'
+            Ensure = 'Present'
+		}
+
+		xDFSNamespaceRoot DFSNamespaceRoot_Domain_DepartmentA
+        {
+            Path                 = '\\contoso.com\departments'
+            TargetPath           = '\\fileserver3\myshare'
+            Ensure               = 'present'
+            Type                 = 'DomainV2'
+            Description          = 'AD Domain based DFS namespace for storing departmental files'
+            TimeToLiveSec        = 600
+            PsDscRunAsCredential = $domainAdminCredentials
+			DependsOn            = "[WindowsFeature]DFS"
+        } 
+
+		xDFSNamespaceRoot DFSNamespaceRoot_Domain_DepartmentB
+        {
+            Path                 = '\\contoso.com\departments'
+            TargetPath           = '\\fileserver2\myshare'
+            Ensure               = 'present'
+            Type                 = 'DomainV2'
+            Description          = 'AD Domain based DFS namespace for storing departmental files'
+            TimeToLiveSec        = 600
+            PsDscRunAsCredential = $domainAdminCredentials
+			DependsOn            = "[xDFSNamespaceRoot]DFSNamespaceRoot_Domain_DepartmentA"
+        } 
+	}
+}
+
+
 Configuration web
 {
  
