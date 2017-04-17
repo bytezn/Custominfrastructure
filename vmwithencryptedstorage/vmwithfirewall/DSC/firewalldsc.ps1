953ea75635767aa62dffce976ddcd2bb1c926f94
Lawrance Reddy <bytezn@hotmail.com>
2017-04-17 14:46:57 +0200

    1

---
+++ vmwithencryptedstorage/vmwithfirewall/DSC/firewalldsc.ps1
@@ -0,0 +1,29 @@
+configuration Main
+{
+    param
+    (
+        [string[]] $NodeName = 'localhost'
+    )
+
+    Import-DSCResource -ModuleName xNetworking
+
+    Node $NodeName
+    {
+        xFirewall Firewall
+        {
+            Name                  = 'NotePadFirewallRule'
+            DisplayName           = 'Firewall Rule for Notepad.exe'
+            Group                 = 'NotePad Firewall Rule Group'
+            Ensure                = 'Present'
+            Enabled               = 'True'
+            Profile               = ('Domain', 'Private')
+            Direction             = 'OutBound'
+            RemotePort            = ('8080', '8081')
+            LocalPort             = ('9080', '9081')
+            Protocol              = 'TCP'
+            Description           = 'Firewall Rule for Notepad.exe'
+            Program               = 'c:\windows\system32\notepad.exe'
+            Service               = 'WinRM'
+        }
+    }
+ }
